17a4a3e2837d94269b514f9bf172d1369ef64889
leo <498242682@qq.com>
2022-07-02 18:57:09 +0800

    init

--- test_case/run_test.py
+++ test_case/run_test.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     for add in os.listdir(TEST_ROOT):
         p = TEST_ROOT.joinpath(add)
         if p.is_dir() and not add.startswith("__"):
-            discover = unittest.defaultTestLoader.discover(p, "test*.py")
+            discover = unittest.TestLoader().discover(p, "test*.py")
             result = runner.run(discover)
             if len(result.errors) > 0:
                 print("FIND ERROR!")
